f84681a8af3005bf04f9721207d0851e225847ef
bekhruzisheee <odiljanovbehruzbek180@gmail.com>
2025-04-25 11:12:22 -0700

    Create strings.py

---
+++ lesson-1/homework/strings.py
@@ -0,0 +1,35 @@
+# %%
+s = 5
+p = 4*s
+a = s**2
+print("Perimeter", p)
+print("Area", a)
+
+# %%
+import math
+d = 8
+length = math.pi * d
+print("Uzunlik", length)
+
+# %%
+a = 5
+b = 6
+ortacha = (a+b)/2
+print("mean", ortacha)
+
+# %%
+a = 12
+b = 24
+sum = a + b
+kopaytma = a*b
+kvadrat_a = a**2
+kvadrat_b = b**2
+print("SUm", sum)
+print("Kopaytma", kopaytma)
+print("a kvadrat", kvadrat_a)
+print("b kvadrat", kvadrat_b)
+
+# %%
+
+
+
